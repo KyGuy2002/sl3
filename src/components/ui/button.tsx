4ff9879cdf31c0c9d0cfbb6e23ad81e1cfff73ab
IEatBeans <ieatbeans222@gmail.com>
2025-03-02 13:05:18 -0800

    fixed shadcn stuff

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,15 +10,15 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "rounded-xl bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "cursor-pointer rounded-xl bg-primary text-primary-foreground shadow hover:bg-primary/90",
         destructive:
-          "rounded-xl bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+          "cursor-pointer rounded-xl bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "rounded-xl border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "cursor-pointer rounded-xl border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "rounded-xl bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "cursor-pointer rounded-xl bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+        ghost: "cursor-pointer hover:bg-accent hover:text-accent-foreground",
+        link: "cursor-pointer text-primary underline-offset-4 hover:underline",
       },
       size: {
         default: "h-9 px-4 py-2",
